ed3aa966c85c9ada482f7fe672c658f0caae970d
DylShero <165214379+DylShero@users.noreply.github.com>
2026-04-30 17:26:33 +0100

    Added parallel implementation of asian option

--- main.cc
+++ main.cc
@@ -5,8 +5,12 @@
 #include <omp.h>
 #include <vector>
 
+
+
 //Function definition for monte carlo simulation
 double monteCarloAsianSerial(int samples, double St0, double strike, double sigma, double r, double T);
+
+double monteCarloAsianParallel(int samples, double St0, double strike, double sigma, double r, double T);
 
 int main(){
     //Example inputs
@@ -28,10 +32,17 @@ int main(){
     //double valueDiffAsianSerial = abs(expectedValue - estimatedPriceSerial);
     double serialAsianTime = endAsianSerial - startAsianSerial;
 
+    double startAsianParallel = omp_get_wtime();
+    double estimatedAsianPriceParallel = monteCarloAsianSerial(samples, St0, strike, sigma, r, T);
+    double endAsianParallel = omp_get_wtime();
+    //double valueDiffAsianSerial = abs(expectedValue - estimatedPriceSerial);
+    double ParallelAsianTime = endAsianParallel - startAsianParallel;
+
     
 
     std::cout << "-----------------------------------" << std::endl;
     std::cout << "Estimated Option Price (Serial): €" << estimatedAsianPriceSerial << " Took: " << serialAsianTime << " seconds. Difference from expected value = " << std::endl;
+    std::cout << "Estimated Option Price (Parallel): €" << estimatedAsianPriceParallel << " Took: " << ParallelAsianTime << " seconds. Difference from expected value = " << std::endl;
    
     return 0;
 }
@@ -74,6 +85,51 @@ double monteCarloAsianSerial(int samples, double St0, double strike, double sigm
     total_payoff = total_payoff/samples;
 
     //Could assume year is just 1 if always using 255 days but here to allow change
+    double T_years = T / 255.0; 
+    double payoff_value = exp(-r * T_years) * total_payoff;
+
+    return payoff_value;
+}
+
+double monteCarloAsianParallel(int samples, double St0, double strike, double sigma, double r, double T) {
+    double dt = 1.0 / 255.0;
+    double mu_t = (r - 0.5 * sigma * sigma) * dt;
+    double step_vol = sigma * sqrt(dt);
+    double total_payoff = 0.0;
+
+
+    //Use reduction to add up total_payoff across all threads
+    #pragma omp parallel for reduction(+:total_payoff)
+    for(int i = 0; i < samples; ++i) {
+        
+        //Seed with i for each thread to have its own PRNG
+        PRNG myGenerator(1 + i); 
+
+        double current_S = St0;
+        double sum_S = St0; //Include starting price in the running sum
+        
+        //Loop through individual days on each path
+        for(int t = 1; t <= T; ++t) {
+            double randVariate = myGenerator.getStandardNormal();
+
+            // Calculate current day's value
+            current_S *= exp(mu_t + step_vol * randVariate);
+            sum_S += current_S; // Sum to total
+        }
+
+        //Caculate average for 255+1 as includes final day so 0-> 255 days
+        double A_T = sum_S / (T + 1.0);
+
+        //Calculate path payoff
+        double path_payoff = std::max(A_T - strike, 0.0);
+        
+
+        total_payoff += path_payoff;
+    }
+    
+    total_payoff = total_payoff / samples;
+
+    //Discount back to present value
     double T_years = T / 255.0; 
     double payoff_value = exp(-r * T_years) * total_payoff;
 
